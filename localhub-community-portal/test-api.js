263dda4f9464832a12d02789e4dac10e86ef8dd1
mkamalhussain <kamalhussain2125@gmail.com>
2025-12-31 00:01:33 +0500

    Clean up directory structure - Remove localhub-community-portal directory from inside imagetoolswebportal - Remove setup-towns.js file that belongs to localhub project - Directory is properly located at E:\GitHub Repos\localhub-community-portal"

--- localhub-community-portal/test-api.js
+++
@@ -1,9 +0,0 @@
-// Quick test to check if the towns API is working
-fetch('http://localhost:3000/api/communities/towns?cityId=1')
-  .then(res => res.json())
-  .then(data => {
-    console.log('API Response:', JSON.stringify(data, null, 2));
-  })
-  .catch(error => {
-    console.error('API Error:', error);
-  });
